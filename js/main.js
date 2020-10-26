152691fccee3e9080e65f2d96220e382b5848aae
tuongnm <tuongnguyenmanh@vccorp.vn>
2020-10-26 23:03:02 +0700

    fix header

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
     $('.bars').click(function() {
-        $('.header-menu, .overlay').addClass('active');
+        $('.list-wrap, .overlay').addClass('active');
     });
-    $('.close-mobile').click(function() {
-        $('.header-menu, .overlay').removeClass('active');
+    $('.close-mobile, .overlay').click(function() {
+        $('.list-wrap, .overlay').removeClass('active');
     });
+
+    if($(window).width() < 992) {
+        $('.submenu-child').slideUp();
+        $('.has-child i').click(function() {
+            $('.submenu-child').toggle();
+        });
+    }
 }); 
